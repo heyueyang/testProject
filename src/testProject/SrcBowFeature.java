105bf8639f19dcb88a6991b638ee63956003993c
niu <408620775@qq.com>
2016-07-15 09:44:16 +0800

    first commit on branch test,the change is add the explaintion of SrcBowFeature

--- src/testProject/SrcBowFeature.java
+++ src/testProject/SrcBowFeature.java
@@ -1,7 +1,11 @@
 package testProject;
 
 import java.util.Map;
-
+/**
+ * 老师的源码提取类,继承自BowFeature.
+ * @author niu
+ *
+ */
 public class SrcBowFeature extends BowFeature{
 	
 	public void buildFeatureMap(String con){
